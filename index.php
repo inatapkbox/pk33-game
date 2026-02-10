100dbdae44102a4b3f6f7452f232c945e5cbb0b2
inatapkbox <akashsebt0@gmail.com>
2026-02-10 16:42:40 +0500

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,9 @@ $primary_keyword = "PK33 Game";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
     <meta name="description" content="<?php echo $meta_description; ?>">
-    <link rel="canonical" href="<?php echo $canonical_url; ?>">
+    <link rel="canonical" href="https://pk33-game-72df2b192cb3.herokuapp.com/">
     <meta name="robots" content="index, follow">
+	<meta name="google-site-verification" content="eG7KBxD0vxVgYg9Im8QuRLRWwZ2HMYLILRndsCzXoC8" />
     
     <!-- Open Graph Tags -->
     <meta property="og:title" content="<?php echo $page_title; ?>">
